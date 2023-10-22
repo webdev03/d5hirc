e7abfb8120594594e918d4d066d61a33d6d4fa04
webdev03 <75148774+webdev03@users.noreply.github.com>
2023-10-22 23:15:02 +1300

    Little patches

--- src/modules/eval.ts
+++ src/modules/eval.ts
@@ -5,16 +5,16 @@ const tioLangs = (await tio.languages()).map(x => x.id);
 export default {
   fn: async (ev) => {
     if(ev.message.startsWith("eval ")) {
-      console.log(ev.ident, "used the eval function!")
+      console.log(ev.nick, "used the eval function!")
       const msgString = ev.message.slice(5);
       const language = msgString.split(" ")[0];
       const evalString = msgString.slice(language.length + 1);
       if(!tioLangs.includes(language)) {
-        ev.reply(template("Eval", ev.ident, `The language ${language} is not supported by TIO.`));
+        ev.reply(template("Eval", ev.nick, `The language ${language} is not supported by TIO.`));
         return;
       }
       if(evalString.length > 120) {
-        ev.reply(template("Eval", ev.ident, `Your code is ${evalString.length - 120} character(s) over the limit.`));
+        ev.reply(template("Eval", ev.nick, `Your code is ${evalString.length - 120} character(s) over the limit.`));
         return;
       }
       const req = await tio.evaluate({
@@ -22,10 +22,10 @@ export default {
         language: language
       }, 7000);
       if(req.status === "timed out") {
-        ev.reply(template("Eval", ev.ident, `Your code timed out.`))
+        ev.reply(template("Eval", ev.nick, `Your code timed out.`))
         return
       }
-      ev.reply(template("Eval", ev.ident, `Output: ${req.output.replace(/\n/gm, "\\n")}`))
+      ev.reply(template("Eval", ev.nick, `Output: ${req.output.replace(/\n/gm, "\\n")}`))
     }
   }
 } satisfies Module;
