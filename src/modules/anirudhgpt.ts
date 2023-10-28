26592d95324f7873ce0368304ad6a8990d723e57
webdev03 <75148774+webdev03@users.noreply.github.com>
2023-10-28 20:11:35 +1300

    Text limiting

--- src/modules/anirudhgpt.ts
+++ src/modules/anirudhgpt.ts
@@ -2,6 +2,14 @@ import config from "../../config";
 import { Module } from "../types";
 import { template } from "../utils";
 
+function limitText(text: string) {
+  if (text.length <= 250) {
+      return text;
+  } else {
+      return text.substring(0, 250) + ` [Continued ${text.length - 250}]`;
+  }
+}
+
 export default {
   fn: async (ev) => {
     if (ev.message.startsWith("gpt ")) {
@@ -23,6 +31,7 @@ export default {
                 content: text
               }
             ],
+            max_tokens: 200,
             temperature: 0.6
           }),
           headers: {
@@ -41,7 +50,7 @@ export default {
       const reqJSON = await req.json();
       const choice = reqJSON.choices[0].message.content as string;
       ev.reply(template("AnirudhGPT", ev.nick, "Here!"));
-      ev.reply(choice.trim());
+      ev.reply(limitText(choice.trim()));
       ev.reply(template("AnirudhGPT", ev.nick, "Done!"));
     }
   }
